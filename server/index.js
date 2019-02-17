0e873c7a0930b4ba9e4bccd89e402cab08fbaf61
Sean Aubin <seanaubin@gmail.com>
2019-02-17 14:27:41 -0500

    remove server code

--- server/index.js
+++ server/index.js
@@ -14,13 +14,13 @@ app.use(cors({
   origin: '*',
 }));
 
+const port = 3000;
+
 // When you sign up for the Face API, your region might be different. Be sure to replace eastus with the correct region.
 const uriBase = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect/';
 
 // When you sign up for the Face API, you'll be given a subscription key. Paste that between the quotation marks below.
 const subscriptionKey = '';
-
-const port = 3000;
 
 app.get('/', (req, res) => (
   res.send('This is the port for receiving images. Your app is running on localhost:4200 - check it out there!'))
@@ -42,26 +42,9 @@ app.post('/', (req, res) => {
       'Ocp-Apim-Subscription-Key' : subscriptionKey
     }
   };
-  request.post(options, (error, response, body) => {
-    res.setHeader('Content-Type', 'application/json');
-    console.log(body);
-    res.send(body);
 
-    if(response.statusCode == "200"){
-      MongoClient.connect(mongoURL, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db(dbName);
-        dbo.createCollection("faces");
-        var myobjFace = { imageUrl: imageUrl, faceAttributes: JSON.stringify(body) };
-        dbo.collection("faces").insertOne(myobjFace, function(err, res) {
-            if (err) throw err;
-            console.log("1 register inserted");
-        });
-
-        db.close();
-      });
-    }
-  });
+  // TODO: send request to Face API
+  // TODO: send Face API response to front-end
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
